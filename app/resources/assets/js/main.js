58e7acc701edc07901d45ed69d4a302405de95ce
Kevin Lee <lee.kev@me.com>
2016-01-29 19:25:07 -0700

    Added mustache for template rendering

--- app/resources/assets/js/main.js
+++ app/resources/assets/js/main.js
@@ -5,7 +5,8 @@
         config = require('config.json'),
         $ = require('jquery'),
         io = require('socket.io-client'),
-        partials = require('../partials/partials.json')
+        Mustache = require('mustache'),
+        Partials = require('../partials/partials.json')
         ;
 
     var mainPage;
@@ -27,36 +28,49 @@
             this.bindListeners();
         },
         preparePage: function (playlist) {
+            var body = $('body').removeClass('loading');
 
+            body.prepend(helpers.renderPartial('main'));
         },
         bindListeners: function () {
 
         }
     };
 
+    var helpers = {
+        renderPartial: function(partial_name, params) {
+            if (!Partials.hasOwnProperty(partial_name)) return '';
+            if (typeof params != 'object') params = {};
+            var partial = Partials[partial_name];
+            Mustache.parse(partial);
+
+            return Mustache.render(partial, params);
+        }
+    };
+
     //TODO remove (currently using for quick testing)
     mainPage.init({
-        "description" : "Your weekly mixtape of fresh music. Enjoy new discoveries and deep cuts chosen just for you. Updated every Monday, so save your favourites!",
-        "external_urls" : {
-            "spotify" : "http://open.spotify.com/user/spotifydiscover/playlist/6yvCTzloWw32VYEuNjeuRU"
+        "description": "Your weekly mixtape of fresh music. Enjoy new discoveries and deep cuts chosen just for you. Updated every Monday, so save your favourites!",
+        "external_urls": {
+            "spotify": "http://open.spotify.com/user/spotifydiscover/playlist/6yvCTzloWw32VYEuNjeuRU"
         },
-        "id" : "6yvCTzloWw32VYEuNjeuRU",
-        "images" : [ {
-            "height" : null,
-            "url" : "https://u.scdn.co/images/pl/default/985687624ed4de4d19cc495cebfd625280d19b10",
-            "width" : null
-        } ],
-        "name" : "Discover Weekly",
-        "owner" : {
-            "external_urls" : {
-                "spotify" : "http://open.spotify.com/user/spotifydiscover"
+        "id": "6yvCTzloWw32VYEuNjeuRU",
+        "images": [{
+            "height": null,
+            "url": "https://u.scdn.co/images/pl/default/985687624ed4de4d19cc495cebfd625280d19b10",
+            "width": null
+        }],
+        "name": "Discover Weekly",
+        "owner": {
+            "external_urls": {
+                "spotify": "http://open.spotify.com/user/spotifydiscover"
             },
-            "href" : "https://api.spotify.com/v1/users/spotifydiscover",
-            "id" : "spotifydiscover",
-            "type" : "user",
-            "uri" : "spotify:user:spotifydiscover"
+            "href": "https://api.spotify.com/v1/users/spotifydiscover",
+            "id": "spotifydiscover",
+            "type": "user",
+            "uri": "spotify:user:spotifydiscover"
         },
-        "uri" : "spotify:user:spotifydiscover:playlist:6yvCTzloWw32VYEuNjeuRU"
+        "uri": "spotify:user:spotifydiscover:playlist:6yvCTzloWw32VYEuNjeuRU"
     });
 
 })();
